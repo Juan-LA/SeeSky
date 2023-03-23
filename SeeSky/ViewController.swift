cb4108d7613f862996395552cb2665a77fc5fe84
Juan David Liut Aymar <liutjuan@gmail.com>
2023-03-23 13:47:37 +0100

    Initial Commit

---
+++ SeeSky/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SeeSky
+//
+//  Created by Juan Liut on 23/03/23.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
