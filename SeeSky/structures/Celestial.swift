0d741b39938e69a45af6dcf76da3a25045f87a05
Computandosw <computando.sw@gmail.com>
2023-04-20 11:25:09 +0200

    added the Celestial struct in a folder

---
+++ SeeSky/structures/Celestial.swift
@@ -0,0 +1,18 @@
+//
+//  Celestial.swift
+//  SeeSky
+//
+//  Created by Marco Di Bernardo on 20/04/23.
+//
+
+import Foundation
+import UIKit
+
+
+struct Celestial {
+    var name: String
+    var image: UIImage
+    var description: String
+    var technical: String
+    
+}
